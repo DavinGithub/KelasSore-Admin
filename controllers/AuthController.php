8d9e4861897b1dd6a3dfae8cf6b05179acac8461
tronout28 <fatihabdurahman28@gmail.com>
2024-12-10 14:14:25 +0700

    [FA] add logic for admin

---
+++ controllers/AuthController.php
@@ -0,0 +1,72 @@
+<?php 
+// File: controllers/AuthController.php
+
+include dirname(__FILE__) . '/../services/database.php';
+include dirname(__FILE__) . '/../models/AuthModel.php';
+
+class AuthController {
+    protected $authModel;
+
+    public function __construct() {
+        $this->authModel = new AuthModel(); // Inisialisasi model
+    }
+
+    // Fungsi untuk login
+    public function login($email, $password) {
+        $admin = $this->authModel->loginUser($email, $password);
+        if ($admin) {
+            session_start(); // Pastikan memulai session sebelum set session
+            // Setelah login berhasil, simpan data pengguna ke session
+            $_SESSION['admin_id'] = $admin['id']; // ID pengguna
+            $_SESSION['admin_email'] = $admin['email']; // Email pengguna
+            $_SESSION['admin_name'] = $admin['name']; // Nama pengguna
+            header('Location: ../pages/dashboard/dashboard.php'); // Sesuaikan path
+            exit();
+        } else {
+            return "Email atau Password salah!";
+        }
+    }
+
+    // Fungsi untuk registrasi
+    public function register($email, $name, $password, $password_confirm) {
+        $message = $this->authModel->registerUser($email, $name, $password, $password_confirm);
+        return $message;
+    }
+
+    // Fungsi untuk logout
+    public function logout() {
+        session_start();
+        session_destroy(); // Menghancurkan session untuk logout
+        header('Location: ../auth/login/login.php'); // Sesuaikan path
+        exit();
+    }
+
+    // Fungsi untuk menangani form login
+    public function handleLoginForm() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $message = $this->login($email, $password);
+            
+            if ($message) {
+                exit();
+            }
+        }
+    }
+
+    // Fungsi untuk menangani form register
+    public function handleRegisterForm() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $email = $_POST['email'];
+            $name = $_POST['name'];
+            $password = $_POST['password'];
+            $password_confirm = $_POST['password_confirm'];
+            $message = $this->register($email, $name, $password, $password_confirm);
+            
+            if ($message) {
+                exit();
+            }
+        }
+    }
+}
+?>
